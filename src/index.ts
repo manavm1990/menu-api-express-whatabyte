af756b928b193ff644ef589213de7d19edf2a024
Manav Misra <manavm@visionwebsoft.com>
2022-04-11 18:41:24 -0500

    Fire up server using port from config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-export const add2Nums = (a: number, b: number): number => a + b;
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import config from './config';
+
+const app = express();
+
+if (!process.env.PORT) {
+  process.exit(1);
+}
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.listen(config.port, () => {
+  console.log(`Listening on port ${config.port}`);
+});
