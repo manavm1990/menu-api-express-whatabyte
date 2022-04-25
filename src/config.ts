c42fa79631aff9ad7681a6b9328f354be7e7e1eb
Manav Misra <manavm@visionwebsoft.com>
2022-04-25 18:48:10 -0500

    Have config.js get type info from global

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,19 @@
 import dotenv from 'dotenv';
+import { Config } from './types/config';
 
 dotenv.config();
 
-export default {
-  port: parseInt(process.env.PORT as string, 10),
+const config: Config = {
+  api: {
+    apiEndpoint: process.env.API_ENDPOINT,
+    apiKey: process.env.API_KEY,
+  },
+  db: {
+    dataSource: process.env.DB_DATA_SOURCE,
+    dataBase: process.env.DB_DATABASE,
+    collection: process.env.DB_COLLECTION,
+  },
+  port: process.env.PORT || 3000,
 };
+
+export default config;
