50f5ba40f71e6cff28b55b9a4414f000966cb0a7
Manav Misra <manavm@visionwebsoft.com>
2022-04-25 19:50:32 -0500

    Add type for MenuItemFilter

--- src/types/menu-item.d.ts
+++ src/types/menu-item.d.ts
@@ -6,4 +6,12 @@ export interface MenuItem {
   image: string;
 }
 
+// Utility Type
 export type NewMenuItemType = Omit<MenuItem, 'id'>;
+
+export type MenuItemFilter =
+  | Partial<MenuItem>
+  | {
+      // TODO: Explore MongoDB indexing for searchable strings, regex, etc.
+      price: Object;
+    };
