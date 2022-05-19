970b7611211a81631a23e250efda6950ba646001
Manav Misra <manavm@visionwebsoft.com>
2022-05-18 19:02:36 -0500

    Add additional CRUD routes

--- src/controllers/items.controller.ts
+++ src/controllers/items.controller.ts
@@ -1,19 +1,64 @@
-import config from '../config';
 import got from 'got';
-import { MenuItem, MenuItemFilter } from '../types/menu-item.d';
+import config from '../config';
+import { MenuItem, MenuItemFilter, NewMenuItem } from '../types/menu-item.d';
 
+interface DeletedItemResponse {
+  deletedCount: number;
+}
+
+const endpoint = config.api.apiEndpoint;
+const headers = {
+  'api-key': config.api.apiKey,
+};
+const dbConfig = {
+  dataSource: config.db.dataSource,
+  database: config.db.dataBase,
+  collection: config.db.collection,
+};
+
+// ⚠️ All things with DATA API are POST requests
 export default {
   find(filter: MenuItemFilter = {}): Promise<MenuItem[]> {
     return got
-      .post(`${config.api.apiEndpoint}/action/find`, {
-        headers: {
-          'api-key': config.api.apiKey,
-        },
+      .post(`${endpoint}/action/find`, {
+        headers,
         json: {
-          dataSource: config.db.dataSource,
-          database: config.db.dataBase,
-          collection: config.db.collection,
+          ...dbConfig,
           filter,
+        },
+      })
+      .json();
+  },
+  create(newMenuItem: NewMenuItem): Promise<MenuItem> {
+    return got
+      .post(`${endpoint}/action/insertOne`, {
+        headers,
+        json: {
+          ...dbConfig,
+          newMenuItem,
+        },
+      })
+      .json();
+  },
+  update(id: string, newMenuItem: NewMenuItem): Promise<MenuItem> {
+    return got
+      .post(`${endpoint}/action/updateOne`, {
+        headers,
+        json: {
+          ...dbConfig,
+          filter: { _id: { $oid: id } },
+          update: { $set: newMenuItem },
+        },
+      })
+      .json();
+  },
+  remove(id: string): Promise<DeletedItemResponse> {
+    return got
+      .post(`${endpoint}/action/deleteOne`, {
+        headers,
+        json: {
+          ...dbConfig,
+          filter: { _id: { $oid: id } },
         },
       })
       .json();
